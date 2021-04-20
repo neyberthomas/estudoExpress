5a1f31793cbd313abe058130db44efd36669fac5
neyberthomas <thomas.neyber@gmail.com>
2021-04-20 01:08:59 +0100

    servidor rodando mas não funcionando

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('E ai meu chapa!')
+const { response } = require('express');
+const express = require('express');
+const { request } = require('node:http');
+const app = express();
+
+let usuarios = [
+    {nome: "Thomas", sobrenome: "Neyber"}
+];
+
+app.get("/inicio", (request, response) => {
+    return response.send(" E ai meu chapa!");
+})
+app.get("/usuarios", (request, response) => {
+    return response.json(usuarios);
+})
+app.listen(3000, () => {
+    console.log("Servidor rodando")
+});
